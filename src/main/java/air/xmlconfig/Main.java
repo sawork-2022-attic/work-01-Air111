b4a5bf5f167b2f599cb6a0fbe1244b44270aa7c8
Air111 <1796389814@qq.com>
2022-03-02 22:05:18 +0800

    complete xml config

--- src/main/java/air/xmlconfig/Main.java
+++ src/main/java/air/xmlconfig/Main.java
@@ -1,5 +1,23 @@
 package air.xmlconfig;
 
+import asciiPanel.AsciiPanel;
+import javax.swing.JFrame;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        AsciiPanel asciiPanel = context.getBean("panel", AsciiPanel.class);      
+        asciiPanel.write("Hello, world!");
+        JFrame frame = new JFrame();
+        frame.add(asciiPanel);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(2);
+        context.close();
+    }
+
 }
